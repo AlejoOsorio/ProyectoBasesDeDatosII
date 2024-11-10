f2f1c7acb86c15a5a771033fab0a5839afdfb8e9
Santiago2004tt <you@example.com>
2024-11-09 20:55:13 -0500

    Se implementaron las nuevas clases relacionadas a la cuenta

---
+++ src/main/java/com/BasesDeDatos/Proyecto/model/entidades/vendedor/Cuenta.java
@@ -0,0 +1,21 @@
+package com.BasesDeDatos.Proyecto.model.entidades.vendedor;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Cuenta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "cuenta_seq")
+    @SequenceGenerator(name = "cuenta_seq", sequenceName = "CUENTA_SEQ", allocationSize = 1)
+    private Integer idCuenta;
+
+    private String usuario;
+    private String password;
+    private Integer idTipoCuenta;
+}
